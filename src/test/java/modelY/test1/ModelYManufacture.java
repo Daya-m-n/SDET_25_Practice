db0581c3e9be0dd2af4cb574edc1f57e6f217606
Dayananda <dayamn123@gmail.com>
2021-12-09 15:50:09 +0530

    First commit

---
+++ src/test/java/modelY/test1/ModelYManufacture.java
@@ -0,0 +1,17 @@
+package modelY.test1;
+
+import org.testng.annotations.Test;
+
+public class ModelYManufacture
+{
+	@Test
+	public void modelY()
+	{
+		System.out.println("Model s assembly started");
+		System.out.println("Model 3 assembly started");
+		System.out.println("Model x assembly started");
+		System.out.println("Model y assembly started");
+
+	}
+
+}
